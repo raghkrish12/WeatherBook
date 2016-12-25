ecf588927bf3e8834491589324b0787cf3f03f66
raghkrish12 <raghulayyaduraikrishnan@Raghuls-MacBook-Pro.local>
2016-12-24 22:53:49 -0500

    adding weather api endpoint

--- js/app.js
+++ js/app.js
@@ -2,16 +2,15 @@ var myApp = angular.module("weatherApp", []);
 
 
 myApp.controller("weatherController", function($scope, $http){
-  $scope.cars = [];
+  $scope.weatherData ;
 
-  (function(){
-    $http.get("js/cars.json").then(
+  $scope.getWeatherInfo = function(cityName){
+    $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName +",uk&appid=85db67b64f975327812dc8db92aa38e0").then(
         function(response){
-          console.log(response);
-          $scope.cars = response.data;
+          $scope.weatherData = response.data;
         },
         function(data){
           console.log("it is en error");
         });
-  })();
+  };
 });
