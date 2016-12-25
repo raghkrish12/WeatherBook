6331ff8d3b10870736958933bfd1c79aff73c71d
raghkrish12 <raghulayyaduraikrishnan@Raghuls-MacBook-Pro.local>
2016-12-25 12:28:41 -0500

    styling

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ var myApp = angular.module("weatherApp", []);
 
 myApp.controller("weatherController", function($scope, $http){
   $scope.weatherData ;
+  $scope.appKey = "85db67b64f975327812dc8db92aa38e0";
 
   $scope.getWeatherInfo = function(cityName){
-    $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName +",uk&appid=85db67b64f975327812dc8db92aa38e0").then(
+    $http.get("http://api.openweathermap.org/data/2.5/weather?q=" + cityName +",uk&appid=" + $scope.appKey).then(
         function(response){
           $scope.weatherData = response.data;
         },
